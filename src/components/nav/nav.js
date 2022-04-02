0c2b608942a5c6c5c3af9b3b7755322902980df1
rniochy <lniochy@gmail.com>
2022-04-02 08:10:02 +0100

    adding nav bar

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -3,9 +3,14 @@ import './nav.css'
 
 const Nav = () => {
     return (
-        <div>
-            Nav
-        </div>
+            <nav>
+                {/* Add ICON with react/icon */}
+                 <a href="#"> Home </a>
+                 <a href="#about">About</a>
+                 <a href="#experience">Experience</a>
+                 <a href="#services">Services</a>
+                 <a href="#contact">Contact</a>
+            </nav>
     );
 }
 
