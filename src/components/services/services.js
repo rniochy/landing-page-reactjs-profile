57de8d8addfb042b62b2056b59badd12ddc23284
rniochy <lniochy@gmail.com>
2022-04-06 04:22:04 +0100

    adding react/icon in the project

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -4,7 +4,116 @@ import './services.css'
 const Services = () => {
     return (
         <section id="services">
-            Services
+             <h5>What i offer</h5>
+             <h2>Services</h2>
+
+             <div className="container services_container">
+                 <article className="service">
+                     <div className="service_head">
+                         <h3> UI/UX</h3>
+                    </div>
+                         <ul className="service_list">
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                         </ul>
+                 </article>
+                 <article className="service">
+                     <div className="service_head">
+                         <h3>Web developement</h3>
+                    </div>
+                         <ul className="service_list">
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                         </ul>
+                 </article>
+                 <article className="service">
+                     <div className="service_head">
+                         <h3>Content creation</h3>
+                    </div>
+                         <ul className="service_list">
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                             <li>
+                                  {/* PUT ICON HEARE */}
+                                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                             </li>
+                         </ul>
+                 </article>
+             </div>
         </section>
     );
 }
